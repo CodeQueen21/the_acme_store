3d6f949aa66b39ef02f797eec6b69fd96dd84d87
Raelinda Shoates <redfire809@gmail.com>
2024-03-11 18:34:56 -0400

    created tables for db

--- server/db.js
+++ server/db.js
@@ -3,6 +3,34 @@ const client = new pg.Client(
   process.env.DATABASE_URL || "postgres://localhost/acme_store_db"
 );
 
+const createTables = async () => {
+  const SQL = `
+  DROP TABLE IF EXISTS favorites;
+  DROP TABLE IF EXISTS users;
+  DROP TABLE IF EXISTS products;
+
+  CREATE TABLE users(
+      id UUID PRIMARY KEY,
+      username VARCHAR(100) UNIQUE NOT NULL,
+      password VARCHAR(255)
+);
+
+CREATE TABLE products(
+    id UUID PRIMARY KEY,
+    name VARCHAR(100)
+  );
+  
+  CREATE TABLE favorites(
+    id UUID PRIMARY KEY,
+    product_id UUID REFERENCES products(id) NOT NULL,
+    user_id UUID REFERENCES users(id) NOT NULL,
+    CONSTRAINT fave_product_user UNIQUE (product_id, user_id)
+  );
+  `;
+  await client.query(SQL);
+};
+
 module.exports = {
   client,
+  createTables,
 };
