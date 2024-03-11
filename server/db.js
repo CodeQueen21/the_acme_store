5ca50f794f3040b1ce6b822639ef56edcd45b79b
Raelinda Shoates <redfire809@gmail.com>
2024-03-11 19:13:16 -0400

    created user and product data

--- server/db.js
+++ server/db.js
@@ -2,6 +2,8 @@ const pg = require("pg");
 const client = new pg.Client(
   process.env.DATABASE_URL || "postgres://localhost/acme_store_db"
 );
+const uuid = require("uuid");
+const bcrypt = require("bcrypt");
 
 const createTables = async () => {
   const SQL = `
@@ -30,7 +32,47 @@ CREATE TABLE products(
   await client.query(SQL);
 };
 
+const createUser = async ({ username, password }) => {
+  const SQL = ` 
+    INSERT INTO users(id, username, password) VALUES($1, $2, $3) RETURNING *
+    `;
+  const response = await client.query(SQL, [
+    uuid.v4(),
+    username,
+    await bcrypt.hash(password, 5),
+  ]);
+  return response.rows[0];
+};
+
+const createProduct = async ({ name }) => {
+  const SQL = `
+ INSERT INTO products(id, name) VALUES($1, $2) RETURNING *
+ `;
+  const response = await client.query(SQL, [uuid.v4(), name]);
+  return response.rows[0];
+};
+
+const fetchProducts = async () => {
+  const SQL = `
+    SELECT * FROM products;
+    `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const fetchUser = async () => {
+  const SQL = `
+    SELECT * FROM users;
+    `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
 module.exports = {
   client,
   createTables,
+  createUser,
+  createProduct,
+  fetchUser,
+  fetchProducts,
 };
--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
-const { client, createTables } = require("./db");
+const {
+  client,
+  createTables,
+  createUser,
+  createProduct,
+  fetchProducts,
+  fetchUser,
+} = require("./db");
 
 const init = async () => {
   await client.connect();
   console.log("connected to database");
   await createTables();
   console.log("tables created");
+  const [Lisa, Brad, James] = await Promise.all([
+    createUser({ username: "Lisa", password: "s3cr3t" }),
+    createUser({ username: "Brad", password: "s3cr3t!!" }),
+    createUser({ username: "James", password: "shhh" }),
+    createProduct({ name: "stove" }),
+    createProduct({ name: "sink" }),
+    createProduct({ name: "blender" }),
+    createProduct({ name: "microwave" }),
+  ]);
+  console.log(await fetchUser());
+  console.log(await fetchProducts());
 };
 
 init();
